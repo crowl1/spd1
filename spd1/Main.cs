89cfde4af7e4e5aaad87001353b6cc628648eb36
crowli <crowli@WIN-FLSGFGEITDV>
2020-02-09 17:27:19 +0200

    доданий клас Main, додані списки до класу Main

---
+++ spd1/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spd1
+{
+    class Main
+    {
+        public List<Storage> storages = new List<Storage>();
+        public List<Order> orders = new List<Order>();
+        public List<Goods> goodss = new List<Goods>();
+        public List<Manager> managers = new List<Manager>();
+        public List<Driver> drivers = new List<Driver>();
+    }
+}
