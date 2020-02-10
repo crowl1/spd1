0602ee26ae74eb738ca88131fc2532f6449ef409
crowli <crowli@WIN-FLSGFGEITDV>
2020-02-10 12:11:50 +0200

    ...

--- spd1/Main.cs
+++ spd1/Main.cs
@@ -11,8 +11,8 @@ namespace spd1
         public List<Storage> storages = new List<Storage>();
         public List<Order> orders = new List<Order>();
         public List<Goods> goodss = new List<Goods>();
-        public List<Manager> managers = new List<Manager>();
-        public List<Driver> drivers = new List<Driver>();
+        List<Manager> managers = new List<Manager>();
+        List<Driver> drivers = new List<Driver>();
 
         string name_driver;
         long time_driver;
